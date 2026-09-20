c4d60fbd56e7aa0fa9beaf47034ee00db60d2bf4
yousef <yousefmpym1@gmail.com>
2026-09-20 15:23:00 +0330

    fixed: cognitive serializers

--- cognick/cognitive/serializers.py
+++ cognick/cognitive/serializers.py
@@ -9,7 +9,30 @@ class CognitiveParameterSerializer(serializers.Serializer):
     Dynamically serializes a CognitiveParameter payload.
     """
 
-    def to_representation(self, instance: CognitiveParameter) -> dict[str, Any]:
+    id = serializers.IntegerField(read_only=True)
+    child_details = serializers.ListField(
+        child=serializers.DictField(),
+        read_only=True,
+    )
+    name_en = serializers.CharField(read_only=True)
+    name_fa = serializers.CharField(read_only=True)
+    name_ar = serializers.CharField(read_only=True)
+    icon = serializers.CharField(
+        allow_null=True,
+        read_only=True,
+    )
+    description_en = serializers.CharField(read_only=True)
+    description_fa = serializers.CharField(read_only=True)
+    description_ar = serializers.CharField(read_only=True)
+    parent = serializers.IntegerField(
+        allow_null=True,
+        read_only=True
+    )
+
+    def to_representation(
+        self,
+        instance: CognitiveParameter,
+    ) -> dict[str, Any]:
         if isinstance(instance, CognitiveParameter):
             return instance.payload.copy()
 
